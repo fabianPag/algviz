8b2ae6e9e01a7737a32780927ad28a949c312fb5
fabianPag <fabbecodes@gmail.com>
2022-12-20 14:31:09 +0100

    Implemented logic for depth-first search

---
+++ src/algorithms/depthfirst.py
@@ -0,0 +1,16 @@
+from collections import deque
+def dfs(graph, node):
+    visited = []
+    stack = deque()
+    
+    visited.append(node)
+    stack.append(node)
+    
+    while stack:
+        s = stack.pop()
+        print(s, end = " ")
+        
+        for n in reversed(graph[s]):
+            if n not in visited:
+                visited.append(n)
+                stack.append(n)
