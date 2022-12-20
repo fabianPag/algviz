1102dc0863bac739f9402d297b02452c5ff53daa
fabianPag <fabbecodes@gmail.com>
2022-12-20 14:22:48 +0100

    General file-structure and UI boilerplate

---
+++ src/algorithms/breadthfirst.py
@@ -0,0 +1,17 @@
+from node import Node
+
+def bfs(graph, startNode):
+    visited = []
+    queue = []
+    
+    visited.append(startNode)
+    queue.append(startNode)
+    
+    while queue:
+        s = queue.pop(0)
+        print(s, end = " ")
+        
+        for n in graph[s]:
+            if n not in visited:
+                visited.append(n)
+                queue.append(n)
